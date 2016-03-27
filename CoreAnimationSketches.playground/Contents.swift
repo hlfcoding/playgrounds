bdbb8b0f9894dbcb1efe85ca5354acb9cc060fa6
Peng Wang <peng@pengxwang.com>
2016-03-26 23:39:59 -0700

    CoreAnimation: port HLFActivityIndicatorView

--- CoreAnimationSketches.playground/Contents.swift
+++ CoreAnimationSketches.playground/Contents.swift
@@ -32,3 +32,62 @@ button.layer.shadowColor = color(.Black)
 button.layer.shadowOffset = CGSizeZero
 button.layer.shadowOpacity = 0.3
 button.layer.shadowRadius = 1.0
+//:
+//: ### Animated Activity Indicator
+//:
+//: A tour of base `CABasicAnimation` and `CAShapeLayer` properties. Some layout boilerplate involved.
+//:
+class ActivityIndicator: UIView {
+
+    var cycleDuration: NSTimeInterval = 1.0
+    var dotCount = 3
+    var fillColor = UIColor.grayColor()
+    var gutterRatio: CGFloat = 0.2
+    var toAlpha: CGFloat = 0.5
+
+    var blockSize: CGFloat { return floor(frame.width / CGFloat(dotCount)) }
+    var dotSize: CGFloat { return floor(blockSize * (1.0 - gutterRatio)) }
+    var duration: NSTimeInterval { return cycleDuration / 2.0 }
+    var gutterSize: CGFloat { return (blockSize - dotSize) / 2.0 }
+    var stagger: NSTimeInterval { return cycleDuration / Double(dotCount) }
+
+    var animations = [CAAnimation]()
+
+    override init(frame: CGRect) { super.init(frame: frame); setUp() }
+    required init?(coder aDecoder: NSCoder) { super.init(coder: aDecoder); setUp() }
+
+    func setUp() {
+        for i in 0..<dotCount {
+            layer.addSublayer(createDotAtIndex(i))
+        }
+    }
+
+    func createDotAtIndex(index: Int) -> CAShapeLayer {
+        let dot = CAShapeLayer()
+        dot.fillColor = fillColor.CGColor
+        let containingRect = CGRect(x: blockSize * CGFloat(index), y: gutterSize, width: dotSize, height: dotSize)
+        dot.path = UIBezierPath(ovalInRect: containingRect).CGPath
+        let animation = createDotAnimationAtIndex(index)
+        dot.addAnimation(animation, forKey: "opacity")
+        animations.append(animation)
+        return dot
+    }
+
+    func createDotAnimationAtIndex(index: Int) -> CABasicAnimation {
+        let animation = CABasicAnimation(keyPath: "opacity")
+        animation.autoreverses = true
+        animation.beginTime = Double(index + 1) * stagger;
+        animation.duration = duration
+        animation.fromValue = NSNumber(float: 1.0)
+        animation.repeatCount = Float.infinity
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        animation.toValue = NSNumber(float: Float(toAlpha))
+        return animation
+    }
+}
+
+let indicator = ActivityIndicator(frame: CGRect(
+    origin: CGPoint(x: button.frame.maxX + gutter, y: gutter),
+    size: CGSize(width: 100.0, height: 30.0)
+))
+stage.addSubview(indicator)
