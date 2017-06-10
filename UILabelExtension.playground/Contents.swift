ba55c8e397d0e4f295803af6d88f9919c733cc59
Peng Wang <peng@pengxwang.com>
2017-06-10 14:11:14 -0700

    UITextExtensions: initial UILabel proof-of-concept

---
+++ UILabelExtension.playground/Contents.swift
@@ -0,0 +1,79 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var isExtensionEnabled = true
+
+extension UILabel {
+
+    func hlf_attributesFromProperties() -> [String : Any] {
+        var attributes = attributedText?.attributes(at: 0, effectiveRange: nil)
+            ?? [String : Any]()
+        attributes[NSFontAttributeName] = font
+        attributes[NSForegroundColorAttributeName] = textColor
+        let paragraphStyle = (
+                attributes[NSParagraphStyleAttributeName] as? NSParagraphStyle
+            )?.mutableCopy() as? NSMutableParagraphStyle
+            ?? NSMutableParagraphStyle()
+        paragraphStyle.alignment = textAlignment
+        attributes[NSParagraphStyleAttributeName] = paragraphStyle
+        return attributes
+    }
+
+    func hlf_pointsByEms(_ ems: CGFloat) -> CGFloat {
+        return round(ems * font.pointSize)
+    }
+
+    func hlf_refreshAttributesText() {
+        // TODO
+    }
+
+    func hlf_setKerning(_ points: CGFloat) {
+        guard let text = text else { return assertionFailure() }
+        var attributes = hlf_attributesFromProperties()
+        attributes[NSKernAttributeName] = points
+        attributedText = NSAttributedString(string: text, attributes: attributes)
+    }
+
+    func hlf_setLineHeight(_ points: CGFloat) {
+        guard let text = text else { return assertionFailure() }
+        var attributes = hlf_attributesFromProperties()
+        let paragraphStyle = attributes[NSParagraphStyleAttributeName]
+            as! NSMutableParagraphStyle
+        paragraphStyle.maximumLineHeight = points
+        attributes[NSParagraphStyleAttributeName] = paragraphStyle
+        attributedText = NSAttributedString(string: text, attributes: attributes)
+    }
+
+}
+
+let label = UILabel()
+label.backgroundColor = .clear
+label.font = .preferredFont(forTextStyle: .title1)
+label.numberOfLines = 0
+label.textAlignment = .center
+label.textColor = .darkText
+label.text = "Hello, playground. You are looking okay."
+
+if isExtensionEnabled {
+    label.text = "Hello, playground. You are looking good."
+    label.hlf_setKerning(-0.5)
+    label.hlf_setLineHeight(label.hlf_pointsByEms(1.1))
+}
+
+let width = label.hlf_pointsByEms(9)
+label.frame.size.width = width
+label.sizeToFit()
+label.frame.size.width = width
+
+import PlaygroundSupport
+
+let container = UIView(
+    frame: label.frame.insetBy(
+        dx: label.hlf_pointsByEms(-1.2), dy: label.hlf_pointsByEms(-1.1)
+    )
+)
+container.backgroundColor = .lightGray
+container.addSubview(label)
+label.center = CGPoint(x: container.bounds.midX, y: container.bounds.midY)
+PlaygroundPage.current.liveView = container
