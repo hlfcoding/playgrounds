9c5d23623064fa68fb51d40cd881872ec889fb2a
Peng Wang <peng@pengxwang.com>
2020-06-21 23:33:06 -0700

    B9 Simulator: continue unit stats, runes

--- B9.playground/Sources/Inventory.swift
+++ B9.playground/Sources/Inventory.swift
@@ -2,18 +2,18 @@ import Foundation
 
 public let sixStarVitalEpic = Rune(
     statType: .vital, valueType: .percent, value: 0.5136, bonusOption: nil
-)
+).validate()
 
 public let sixStarVitalEpicFlatSubstatVitalPercent = Rune(
     statType: .vital, valueType: .flat, value: 1344,
     bonusOption: Rune.BonusOption(statType: .vital, valueType: .percent, value: 0.08)
-)
+).validate()
 
 public let fiveStarAssaultLegendFlat = Rune(
     statType: .assault, valueType: .flat, value: 321, bonusOption: nil
-)
+).validate()
 
 public let fiveStarAssaultEpicFlatSubstatVitalPercent = Rune(
     statType: .assault, valueType: .percent, value: 0.4036,
     bonusOption: Rune.BonusOption(statType: .vital, valueType: .flat, value: 192)
-)
+).validate()
--- B9.playground/Sources/Rune.swift
+++ B9.playground/Sources/Rune.swift
@@ -12,6 +12,10 @@ public struct Rune: RuneLike {
 
     public enum StatType {
         case assault, vital, fatal, rage, shield
+        func supports(valueType: ValueType) -> Bool {
+            guard valueType == .flat else { return true }
+            return self == .fatal || self == .rage || self == .shield
+        }
     }
     var statType: StatType
 
@@ -29,18 +33,28 @@ public struct Rune: RuneLike {
     }
     let bonusOption: BonusOption?
 
+    public func validate() -> Rune {
+        assert(statType.supports(valueType: valueType))
+        return self
+    }
+
     public static func valueToAdd(
         from allRunes: [Rune], of statType: StatType, to baseValue: Int
     ) -> (value: Int, newBaseValue: Int?) {
-        var runeLikes = [RuneLike]()
-        runeLikes += allRunes.filter { $0.statType == statType } as [RuneLike]
-        runeLikes += allRunes.compactMap { $0.bonusOption }.filter { $0.statType == statType } as [RuneLike]
+        let runeLikes = self.runeLikes(from: allRunes, of: statType)
         var value: Double = 0
         value += runeLikes.filter { $0.valueType == .flat }.reduce(0) { $0 + $1.value }
         let newBaseValue: Int? = (value > 0) ? (baseValue + Int(value)) : nil
         value += (Double(baseValue) + value) *
             runeLikes.filter { $0.valueType == .percent }.reduce(0) { $0 + $1.value }
         return (Int(value), newBaseValue)
+    }
+
+    static func runeLikes(from allRunes: [Rune], of statType: StatType) -> [RuneLike] {
+        var runeLikes = [RuneLike]()
+        runeLikes += allRunes.filter { $0.statType == statType } as [RuneLike]
+        runeLikes += allRunes.compactMap { $0.bonusOption }.filter { $0.statType == statType } as [RuneLike]
+        return runeLikes
     }
 
 }
