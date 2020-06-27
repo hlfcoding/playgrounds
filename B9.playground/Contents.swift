71e75cea9b74c4816af0faa63c8554e462546c8d
Peng Wang <peng@pengxwang.com>
2020-06-26 23:15:15 -0700

    B9 Simulator: continue unit stats, runes

--- B9.playground/Contents.swift
+++ B9.playground/Contents.swift
@@ -54,36 +54,41 @@ struct Unit {
 
 }
 
-let levia = Unit(
-    baseAttack: 669, baseCritDamage: 0.5, baseCritRate: 0.2, baseDefense: 0, baseHP: 4444,
-    runes: [.sixStarVitalEpic, .sixStarVitalEpicFlatSubstatVitalPercent],
-    soulGearBuffs: nil
-)
+extension Unit {
 
-assert(levia.attack == 669)
-assert(levia.hp == 9223)
+    static let angelica = Unit(
+        baseAttack: 1975, baseCritDamage: 1, baseCritRate: 0.25, baseDefense: 0.1, baseHP: 4285,
+        runes: [.sixStarFatalLegend, .fiveStarShieldLegendSubstatFatal],
+        soulGearBuffs: .maxedWarrior
+    )
 
-let scarlet = Unit(
-    baseAttack: 738, baseCritDamage: 0.75, baseCritRate: 0.35, baseDefense: 0.05, baseHP: 3115,
-    runes: [.fiveStarAssaultLegendFlat, .fiveStarAssaultEpicFlatSubstatVitalPercent],
-    soulGearBuffs: .maxedWarrior
-)
+    static let levia = Unit(
+        baseAttack: 669, baseCritDamage: 0.5, baseCritRate: 0.2, baseDefense: 0, baseHP: 4444,
+        runes: [.sixStarVitalEpic, .sixStarVitalEpicFlatSubstatVitalPercent],
+        soulGearBuffs: nil
+    )
 
-assert(scarlet.attack == 1592)
-assert(scarlet.hp == 3638)
+    static let scarlet = Unit(
+        baseAttack: 738, baseCritDamage: 0.75, baseCritRate: 0.35, baseDefense: 0.05, baseHP: 3115,
+        runes: [.fiveStarAssaultLegendFlat, .fiveStarAssaultEpicFlatSubstatVitalPercent],
+        soulGearBuffs: .maxedWarrior
+    )
 
-let angelica = Unit(
-    baseAttack: 1975, baseCritDamage: 1, baseCritRate: 0.25, baseDefense: 0.1, baseHP: 4285,
-    runes: [.sixStarFatalLegend, .fiveStarShieldLegendSubstatFatal],
-    soulGearBuffs: .maxedWarrior
-)
+}
 
-assert(angelica.attack == 2173)
-assert(angelica.critRate == 0.8346)
-assert(angelica.defense == 0.3962)
-assert(angelica.hp == 4714)
+assert(Unit.levia.attack == 669)
+assert(Unit.levia.hp == 9223)
 
-print(angelica.normalAttack)
+assert(Unit.scarlet.attack == 1592)
+assert(Unit.scarlet.hp == 3638)
+
+assert(Unit.angelica.attack == 2173)
+assert(Unit.angelica.critRate == 0.8346)
+assert(Unit.angelica.defense == 0.3962)
+assert(Unit.angelica.hp == 4714)
+
+var subject = Unit.angelica
+print(Unit.angelica.normalAttack)
 
 struct Round {
 
